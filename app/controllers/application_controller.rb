7a41756f39ec5f545aeeb48bce70e626f7a732f2
joseph mwanzia <makamwanzia@gmail.com>
2023-03-07 16:53:13 +0300

    Added signup method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,28 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/pets" do
+    pets = Pet.all
+    pets.to_json
   end
 
+  get "/users" do
+    users = User.all
+    users.to_json
+  end
+
+  post "/signup" do
+    #check fields are filled
+    if params[:name].blank? || params[:email].blank? || params[:password].blank?
+      redirect "/signup"
+
+      #check if username is taken
+    elsif user.find_by( name: params[user][:name])
+      redirect "/signup"
+    else
+      User.create(params[:user])
+    end
+    binding.pry
+
+  end
 end
