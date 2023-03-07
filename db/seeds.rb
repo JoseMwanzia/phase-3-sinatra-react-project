22fdcae2c2aeba783275adc7e791a9d7d1315df8
joseph mwanzia <makamwanzia@gmail.com>
2023-03-07 21:25:32 +0300

    Changed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,21 @@ puts "🌱 Seeding spices..."
 # Seed your database here
 # User information
 
-# 'rake db:seed' aborted.
 
-User.create(username:"user20",  password:"password20")
-User.create(username:"user45",  password:"password45")
-User.create(username:"user60",  password:"password60")
-User.create(username:"user9",   password:"password9")
-User.create(username:"user31",  password:"password31")
-User.create(username:"user66",  password:"password66")
+User.create(username:"user1",password:"password20",email:"kelvin@gmail.com")
+User.create(username:"user2",password:"password45",email:"joe@gmail.com")
+User.create(username:"user3",password:"password60",email:"kelvinKlein@gmail.com")
+User.create(username:"user4",password:"password9",email:"kel@gmail.com")
+User.create(username:"user5",password:"password31",email:"joan@gmail.com")
+User.create(username:"user6",password:"password66",email:"lucy@gmail.com")
 
 # Pets information
-Pet.create(name:"Luna",description:"A friendly and playful golden retriever", breed:"Affenpinscher", status:"Adopted", age:9, image_url:"https://www.shutterstock.com/image-photo/adorable-welsh-springer-spaniel-dog-breed-1823409425")
-Pet.create(name:"Simba",description:"A shy and sweet border collie", breed:"Alaskan ", status:"Not Adopted", age:3, image_url:"https://www.shutterstock.com/image-photo/hungry-funny-puppy-dog-licking-nose-1805010616")
-Pet.create(name:"Milo",description:"A loyal and smart German shepherd", breed:"American Malamute", status:"Adopted", age:5, image_url:"https://www.shutterstock.com/image-photo/cute-puppy-paws-over-white-sign-412321696")
-Pet.create(name:"Zeus",description:"A cute and cuddly poodle", breed:"Australian", status:"Not Adopted", age:1, image_url:"https://www.shutterstock.com/image-photo/tabby-cat-border-collie-dog-front-1934050523")
-Pet.create(name:"Chloe",description:"A brave and energetic boxer", breed:"Hound", status:"Adopted", age:10, image_url:"https://www.shutterstock.com/image-photo/cute-corgi-dog-looking-laptop-glasses-1715597689")
-Pet.create(name:"Oliver",description:"A gentle and loving labrador", breed:"German shepherd", status:"Not Adopted", age:7, image_url:"https://www.shutterstock.com/image-photo/amazing-portrait-young-crossbreed-dog-german-706603864")
+Pet.create(name:"Frezco",description:"Just a happy dog",breed:"Doberman",status:"Available",age:3,image_url:"https://images.pexels.com/photos/12455804/pexels-photo-12455804.jpeg?auto=compress&cs=tinysrgb&w=800")
+Pet.create(name:"Buddy",description:"A friendly and playful golden retriever",breed:"Golden Retriever",status:"Available",age:3,image_url:"https://images.pexels.com/photos/1001976/pexels-photo-1001976.jpeg?auto=compress&cs=tinysrgb&w=600")
+Pet.create(name: "Luna",description:"A shy and sweet border collie",breed:"Border Collie",status:"Adopted",age:2,image_url:"https://images.pexels.com/photos/3041659/pexels-photo-3041659.jpeg?auto=compress&cs=tinysrgb&w=600")
+Pet.create(name:"Max",description:"A loyal and smart German shepherd",breed:"German Shepherd",status:"Available",age:4,image_url:"https://images.pexels.com/photos/3709026/pexels-photo-3709026.jpeg?auto=compress&cs=tinysrgb&w=600")
+Pet.create(name:"Bella",description:"A cute and cuddly poodle",breed:"Poodle",status:"Adopted",age:1,image_url:"https://images.pexels.com/photos/7516820/pexels-photo-7516820.jpeg?auto=compress&cs=tinysrgb&w=600")
+Pet.create(name:"Rocky",description:"A brave and energetic boxer",breed:"Boxer",status:"Available",age:5,image_url:"https://images.pexels.com/photos/1294062/pexels-photo-1294062.jpeg?auto=compress&cs=tinysrgb&w=600")
+
 
 puts "✅ Done seeding!"
