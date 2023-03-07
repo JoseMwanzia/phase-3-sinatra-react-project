da3f47ffd7e5fb96d00c1cc481ee23b08fe63d75
joseph mwanzia <makamwanzia@gmail.com>
2023-03-07 21:32:03 +0300

    Updated pets table with more columns

--- db/migrate/20230306104702_create_pets.rb
+++ db/migrate/20230306104702_create_pets.rb
@@ -3,7 +3,10 @@ class CreatePets < ActiveRecord::Migration[6.1]
     #create columns for pets
     create_table :pets do |t|
       t.string :name
+      t.string :description
       t.string :breed
+      t.string :status
+      t.string :age
       t.string :image_url
       t.integer :user_id
     end
